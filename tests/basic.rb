2d815a50d1755921e53e8c7e739fcf3f92699885
Leon Mika <lmika314@gmail.com>
2014-02-18 16:46:58 +1100

    Added a multi-chapter test.

--- tests/basic.rb
+++ tests/basic.rb
@@ -31,4 +31,28 @@ class TestBasic < Minitest::Test
 
         assert File.file?(TEST_DIR + "/book1/chap0000.xhtml"), "First chapter present"
     end
+
+    ###
+    # Test the creation of a single book with a few chapters.
+    def test_more_chapters
+        Republic::DirBinder.write(Republic::Book.new { |b|
+            b << Republic::HtmlChapter.new { |c| 
+                c.text = "<p>This is chapter 1</p>"
+            }
+            b << Republic::HtmlChapter.new { |c| 
+                c.text = "<p>This is chapter 2</p>"
+            }
+            b << Republic::HtmlChapter.new { |c| 
+                c.text = "<p>This is chapter 3</p>"
+            }
+        }, TEST_DIR + "/book2")
+
+        assert File.file?(TEST_DIR + "/book2/chap0000.xhtml"), "First chapter present"
+        assert File.file?(TEST_DIR + "/book2/chap0001.xhtml"), "Second chapter present"
+        assert File.file?(TEST_DIR + "/book2/chap0002.xhtml"), "Third chapter present"
+
+        assert_match /.*This is chapter 1.*/, IO.read(TEST_DIR + "/book2/chap0000.xhtml")
+        assert_match /.*This is chapter 2.*/, IO.read(TEST_DIR + "/book2/chap0001.xhtml")
+        assert_match /.*This is chapter 3.*/, IO.read(TEST_DIR + "/book2/chap0002.xhtml")
+    end
 end
